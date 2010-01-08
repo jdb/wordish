10a65c56ef6842d1b7577da7423ddb0c56127b98
Jean Daniel Browne <jdbrowne@anevia.com>
2010-01-08 18:08:54 +0100

    Doctests

--- wordish.py
+++ wordish.py
@@ -5,47 +5,13 @@ from shlex import shlex
 from itertools import takewhile
 from StringIO import StringIO
 
-def lex ( s, shlex_object=False, com='', whi='' ):
-    """
-    Returns the list of tokens of the input string.
-
-    The token commenters and whitespace are set to the empty string
-    and can be modified with the function arguments 'com' and
-    'whi'. 
-
-    If the argument shlex_object is set to True then it'is not the
-    list of tokens but the shlex object itself so that you can
-    experiment with the :obj:`shlex` and it multiple attribute and
-    method.
-
-    >>> lex( "Yozza 1,2" )
-    ['Yozza', ' ', '1', ',', '2']
-
-    >>> tokens = lex( "1 2", shlex_object=True )
-    >>> tokens.whitespace = ' '
-    >>> [t for t in tokens ]
-    ['1', '2']
-
-    >>>> lex( "Yozza # you dont want to see that", whi=' ', com='#' )
-    ['Yozza']
-    """
-
-    tokens = shlex( StringIO( s )) 
-    tokens.commenters = com         
-    tokens.whitespace = whi         
-
-    if shlex_object:
-        return tokens
-    else:
-        return list( tokens )
-
 class ShellSession( object ):
     """
     An iterator which parses a text file of a shell session and yields
     pairs of commands and outputs
     
-    >>> session = ShellSession( StringIO( "~$ echo coucou\ncoucou" ))
-    >>> for c,o in session: print c,o
+    >>> sess = ShellSession( StringIO( "~$ echo coucou\ncoucou" ))
+    >>> for c,o in sess: print c,o
     ...
     echo coucou coucou
     """
@@ -63,104 +29,181 @@ class ShellSession( object ):
         self.tokens = shlex( f if hasattr(f, "read") else file( f ) )
 
         self.tokens.commenters = '' 
-        # comments are broken for commands, le retour chariot qui signifie
-        # la fin d'une commande est consomme par les commentaires, la
-        # commande est incorrectement termine.
-    
+        # deactivate shlex comments facility which won't work for us.
+        # The terminating linefeed means two things: end of comment an
+        # end of command. As shlex consume the terminating linefeed,
+        # there is no end of command left.
+
         self.tokens.whitespace = ''
         # characters cited in ``shlex.whitespace`` are not returned by
         # get_token. If empty, whitespaces are returned as is which is
         # what we want: they definitely count in bash, and may count in
         # output, so in just do not munge them.
 
+        self.nested = 0
+
     def has_token( self ):
         """
-        Return True when there are token available, False if the
-        stream token is empty.
+        Return True when there are still tokens available, False if
+        the stream token is empty.
 
-        >>> session = ShellSession( s="~$ env | grep USER\nUSER=jd" ))
-        >>> session.has_token()
+        >>> sess = ShellSession( s="~$ env | grep USER\nUSER=jd" ))
+        >>> sess.has_token()
         True
-        >>> for c,o in session: pass
+        >>> for t in sess.tokens: pass
         >>> sesssion.has_token()
         False
         """
         t = self.tokens.get_token()
         return False if t==self.tokens.eof else self.tokens.push_token( t ) or True
 
-
-    def get_command( self, nested=0 ):
+    def end_of_output( self, t ):
         """
-        Returns the command i.e. everything except comments, until a
-        linefeed except when the linefeed is nested in parentheses and
-        brackets.
+        Returns true if the token is after the last output token, else
+        returns false. The functions stops right before the prompt
+        which can be either '~# ' or '~$ '.
 
-        >>> session = ShellSession( s="true")
-        >>> session.get_command()
-        true
-        >>> ShellSession( s="true\n" ).get_command()
-        true
-        >>> ShellSession( s="(true\n)" ).get_command()
-        (true
+        >>> sess = session( s="youpi\n~# " )
+        >>> sess.tokens.next()
+        youpi
+        >>> sess.end_of_output( 'youpi' )
+        False
+        >>> sess.tokens.next()
+        '~'
+        >>> sess.end_of_output( '~' )
+        True
+        """
+
+        if t=='~':
+            n1, n2 = [ self.tokens.next(), self.tokens.next() ]
+            if n1 in '$#' and n2 == ' ':
+                return True
+            else:
+                self.tokens.push_token( n1 ) 
+                self.tokens.push_token( n2 )
+                return False
+        else:
+            return False
+
+    def end_of_command( self, t ):
+        """
+        Returns true if the token is after the last comand token, else
+        returns false. The functions stops right before the linefeed
+        only when the token is not nested in brackets or parenthesis.
+
+        >>> sess = session( s="youpi\n~# " )
+        >>> sess.tokens.next()
+        youpi
+        >>> sess.end_of_output( 'youpi' )
+        False
+        >>> sess.tokens.next()
+        '~'
+        >>> sess.end_of_output( '~' )
+        True
+        """
+
+        t = self.tokens.next()
+        if t == '\n' or t == '#' or t==self.tokens.eof:
+            if t=='#': 
+                list( takewhile( lambda t:t!='\n', self.tokens ) )
+                t = '\n'
+	
+                if self.nested==0:
+                    return True
+        else:
+
+            if   t in '({': self.nested += 1
+            elif t in '})': self.nested -= 1
+
+            return False
+
+
+
+    def take_until( self, terminator ):
+        """
+        Returns a command or an output depending of the terminator
+        provided. i.e. every token on which the terminator returns
+        False. The terminator is a function which take a token and
+        returns whether the stream of tokens is terminated or not.
+
+        >>> session = ShellSession
+        >>> sess = session( s="cmd" )
+        >>> sess.take_until( sess.end_of_command )
+        cmd
+        >>> session( s="cmd2\ncmd" ).take_until( sess.end_of_command )
+        cmd
+        >>> session( s="(cmd\n)\ncmd2" ).take_until( sess.end_of_command )
+        (cmd
         )
-        >>> ShellSession( s="t () {\ntrue\n)" ).get_command()
+        >>> session( s="t () {\ncmd\n)\ncmd2" ).take_until( sess.end_of_command )
         t () {
         true
         }
         """
-
-        c = []
-
-        for t in self.tokens:
-            
-            if t == '\n' or t == '#' or t==self.tokens.eof:
-                
-                if t=='#': 
-                    
-                    [ t for t in takewhile( lambda t:t!='\n', self.tokens )]
-                    t = '\n'
-	
-                if nested==0:
-                    return ''.join(c).strip()
-	
-            elif t in '({': nested += 1
-            elif t in '})': nested -= 1
-	
-            c.append( t )
-
-        return ''.join(c).strip()
-	
-    def get_output( self ):
-        """
-        Returns the output i.e. everything until the prompt ``~$``, or ``~#``
-        terminated by a mandatory space.
-        """
         
-        o = []
-
-        for t in self.tokens:
+        return ''.join (
+            [ t for t in self.tokens if not terminator( t ) ]
+            ).strip()
             
-            if t=='~':
-                n1, n2 = [ self.tokens.next(), self.tokens.next() ]
-                if n1 in '$#' and n2 == ' ':
-                    return ''.join( o ).strip()
-                else:
-                    self.tokens.push( n1 ) 
-                    self.tokens.push( n2 )
-                    
-            o.append( t )
-
-        return ''.join( o ).strip()
 
     def __iter__(self):
         return self
 
     def next(self):
-        self.get_output()
-        while self.has_token() :
-            return self.get_command(), self.get_output() 
-        raise StopIteration
+        """
+        Returns the next pair of command and output.
 
+        >>> session = ShellSession
+        >>> sess = session( s="~# cmd\noutput\n~# true\n" )
+        >>> for c,o in sess: print "command: %s\noutput: %o" % (c,o)
+        command: cmd
+        output: output
+        command: true
+        output: 
+        """
+
+        self.take_until( self.end_of_output ) 
+
+        while self.has_token() :
+            return ( self.take_until( self.end_of_command ), 
+                     self.take_until( self.end_of_output ) ) 
+        else:
+            raise StopIteration
+
+
+def lex ( s, shlex_object=False, com='', whi='' ):
+    """
+    Returns the list of tokens of the input string.
+
+    The token commenters and whitespace are set to the empty string
+    and can be modified with the function arguments 'com' and
+    'whi'. 
+
+    If the argument shlex_object is set to True then it'is not the
+    list of tokens but the shlex object itself so that you can
+    experiment with the :obj:`shlex` and it multiple attribute and
+    method.
+
+    >>> lex( "Yozza 1 2" )
+    ['Yozza', ' ', '1', ' ', '2']
+
+    >>> tokens = lex( "Yozza 1 2", shlex_object=True )
+    >>> tokens.whitespace = ' '
+    >>> [t for t in tokens ]
+    ['Yozza', '1', '2']
+
+    >>>> lex( "Yozza # a comment you dont want to see", whi=' ', com='#' )
+    ['Yozza']
+    """
+
+    tokens = shlex( StringIO( s )) 
+    tokens.commenters = com         
+    tokens.whitespace = whi         
+
+    if shlex_object:
+        return tokens
+    else:
+        return list( tokens )
 
 class SubShell ( object ):
     """
@@ -175,11 +218,12 @@ class SubShell ( object ):
     ...
     coucou
     2
-
-    Note: for this kind of interactive stuff, maybe a pty is more
-    appropriate. ssh does that and bash behaves a bit differently when
-    run hooked to a pty.    
     """
+
+    # Note: for this kind of interactive stuff, maybe a pty is more
+    # appropriate. ssh does that and bash behaves a bit differently when
+    # run hooked to a pty.    
+
 
     # __init__
     # kill_on_exit=False
@@ -226,7 +270,6 @@ if __name__=="__main__":
     if len(sys.argv)>1 :
         files = sys.argv[1:] 
     else:
-        from StringIO import StringIO
         files = StringIO("""
 ~$ echo toto
 toto
@@ -261,17 +304,11 @@ Yo
 
 
 #     for f in files:
-
 #         session = ShellSessionParser( f )
-
 #         with SubShell() as shell:
-
 #             for c,o in session:
-
 #                 print( "Trying:\n\t%s\nExpecting:\n\t%s" % ( c, o ) )
-
 #                 ans = shell.call( c )
-
 #                 print( "%s" % ( "ok" if o==ans else format_error( c, o, ans ) ) )
 
                 # if ans.err != '':
