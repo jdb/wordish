d7b1be20be4080fb1d057c0da965946cf4f54f07
jdb <jeandaniel.browne@gmail.com>
2010-02-02 18:27:47 +0100

    better handling of the doctree (traverse is what I needed)

--- extract_shell_snippets_from_rst.py
+++ extract_shell_snippets_from_rst.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python
 
-"""
-
-"""
-
-
-from docutils.nodes import literal_block, Text
-from docutils.parsers import rst
-
-class SourceCode( rst.Directive ):
-
-    has_content = True
-    def run(self):
-        
-        self.assert_has_content()
-        return [ literal_block( text='\n'.join(self.content[2:] ))]
-
-from docutils.parsers.rst import directives
-directives.register_directive( "sourcecode", SourceCode )
-
 try:
     import locale
     locale.setlocale(locale.LC_ALL, '')
 except:
     pass
 import sys
-from docutils.core import publish_doctree
+from pprint import pprint
+from docutils import core, nodes
+from docutils.parsers import rst
 
-from lxml.etree import parse
 
-for i in publish_doctree( file( sys.argv[1] ).read() ):
+class SourceCode( rst.Directive ):
+
+    has_content = True
+
+    def run( self ):
+        self.assert_has_content()
+        
+        return [ nodes.literal_block( text='\n'.join(self.content[2:] )) ]
+
+rst.directives.register_directive( "sourcecode", SourceCode )
+
+for i in core.publish_doctree(file(sys.argv[1]).read()).traverse():
     if i.tagname=='literal_block':
         print i.astext()
+
 
