37f7afde9121907dab4f52a9c09cea919ed32797
Jean Daniel Browne <jeandaniel.browne@gmail.com>
2010-02-21 21:51:20 +0100

    Fixed the setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     py_modules = [ 'wordish' ],
 
     name = 'wordish',
-    version = file('version').read().strip(),
+    version = "1.0.1",
     author = 'Jean Daniel Browne',
     author_email = 'jeandaniel.browne@gmail.com',
     description = ("Parses a shell session, test the "
